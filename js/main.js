b0f310743b322ac9f4aa37ffa381dca72397f624
Prat0487 <30476260+Prat0487@users.noreply.github.com>
2025-02-13 22:44:50 +0530

    remove unused codefiles (#38)
    * added more detials button on every product
    
    * fixed clicking more details button
    
    * moved data and js folder from assets to root and removed irrelevant and backup folder and moved to different repo
    
    * fixed product-sportsgoods.html page to display relevant attributes for sports-wear
    
    * fixed product-detail.html page to show product-detail correctly
    
    * Increased  size of image in product-detail.html page and also include relevant images
    
    * fixed nav page to show sports wear and also provided actual values in sports json
    
    * restore silvercraft.html functionality
    
    * changed logo and revamp colour theme for entire website
    
    * modified silver article to womenswear
    
    * changed banner content of mens and womens product page
    
    * changed product type from silvercraft to women everywhere without loosing the functionality
    
    * changed product type to men instead of sports
    
    * added categories in mens and womens page
    
    * fixed font and color of mens, women and detail html pages
    
    * modified  filter category in womenwear html file
    
    * added attribute category in menswear
    
    * added category filter in menswear
    
    * added search by attributes
    
    * removed filter weight and replaced with grammage
    
    * added sort filter in menswear
    
    * changed index page and fix footer contents
    
    * added zoomin effect for product detail page
    
    * added zoom in effect for mens and women html page
    
    * added products for men
    
    * added photos for mens short sleeve wear
    
    * done cleanup
    
    * added undershirt for men
    
    * removed jacket category filter for men and added Undershirt
    
    * added Shorts products for Menswear
    
    * added filter Shorts
    
    * fix menswear json file
    
    * fix loading
    
    * updated phone and email
    
    * removed space from phone
    
    * updated href phone
    
    * fixed teams page
    
    * add appscript to do data collection and fix footer
    
    * added Pants for Men
    
    * added Jackets for men
    
    * added Jackets and Pants as Categories in menswear
    
    * fixed README.md file
    
    * added robot.txt to improve search engine indexing
    
    * Improved SEO ranking
    
    * added google analytics
    
    * added sitemap and breadcrump
    
    * improved performance of website
    
    * added women product -sport bra and sports top
    
    * added category filter Sports Bra and Sports Top for womenswear and added Anant Mittal as CEO
    
    * added keywords for SEO in index, menswear, womenswear page
    
    * image optimisation done
    
    * restore header tag in index page
    
    * improve mobile loading
    
    * fixed mobile optimisation css
    
    * deleted unused files
    
    * remove unused code
    
    * removed admin folder as sitemap is not needed without backend implementation

--- js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const loader = new ProductLoader();
-  const products = await loader.loadProducts('women');
-  loader.renderProducts(products, 'productGrid');
-});
